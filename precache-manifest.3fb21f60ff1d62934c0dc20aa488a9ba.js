5ce7f4ad1961a0d36a89531f0171421ddd3e714d
amanmakwana-4 <ma5066770@gmail.com>
2024-09-25 20:36:44 +0530

    Updates

---
+++ precache-manifest.3fb21f60ff1d62934c0dc20aa488a9ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac29720cf9564b4b3c086fa464cd7b26",
+    "url": "/Word-Counter/index.html"
+  },
+  {
+    "revision": "4f6d121d45979d7580f0",
+    "url": "/Word-Counter/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/Word-Counter/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Word-Counter/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "4f6d121d45979d7580f0",
+    "url": "/Word-Counter/static/js/main.4d070cf2.chunk.js"
+  },
+  {
+    "revision": "172cec96ae4602fea654",
+    "url": "/Word-Counter/static/js/runtime~main.42e4a96a.js"
+  }
+]);
